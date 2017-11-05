b4ff5dc69cfe1822308a2204fa63645ee67dbc75
tmgearedup8297 <32619089+tmgearedup8297@users.noreply.github.com>
2017-11-04 22:47:58 -0400

    Test V1 of Ritvik's func
    Wrote an untested version of a function. The function drops a block on top of another block and picks up both blocks.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servotest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servotest.java
@@ -54,7 +54,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="GlyphTest", group="Iterative Opmode")
+@TeleOp(name="GlyphGrabberTest", group="Iterative Opmode")
 //@Disabled
 public class servotest extends OpMode
 {
