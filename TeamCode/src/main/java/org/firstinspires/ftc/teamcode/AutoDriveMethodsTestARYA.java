7c76adb87d9c0a1d9953bc86ad4977a5d68d0a22
tmgearedup8297 <32619089+tmgearedup8297@users.noreply.github.com>
2018-01-28 14:40:04 -0500

    Strafing mostly works, some bugs
    Robot strafes bc of monitoring a pair of wheels diagonal to each other and adjusting power settings to account for slipping gears. Still have to work on gyro correction

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveMethodsTestARYA.java
@@ -0,0 +1,460 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import java.util.Locale;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Auto Drive methods test", group="Linear Opmode")
+//@Disabled
+public class AutoDriveMethodsTestARYA extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    private DcMotor leftFront = null;
+    private DcMotor rightFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightBack = null;
+
+    private final int TICKS_PER_INCH=36;
+
+
+    private int[] zeroPos = {0, 0, 0, 0};
+    private double[] targetDist = {24, 24, 24, 24};
+    private int[] targetClicks = {0, 0, 0, 0};
+
+    // The IMU sensor object
+    BNO055IMU imu;
+
+    // State used for updating telemetry
+    private Orientation angles;
+    private Acceleration gravity;
+
+
+
+    //----------------------------------------------------------------------------------------------
+    // Main logic
+    //----------------------------------------------------------------------------------------------
+
+    @Override public void runOpMode() {
+
+
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+        /**/
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        //shoulder.setPosition(spos);
+        // elbow.setPosition(epos);
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // Set up our telemetry dashboard
+        //composeTelemetry();
+
+        waitForStart();
+
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        // Loop and update the dashboard
+
+        // Wait for the game to start (driver presses PLAY)
+
+        runtime.reset();
+
+        strafeDistRight(25, 25, 25, 25);
+        strafeDistLeft(25, 25, 25, 25);
+        //}
+
+
+        // run until the end of the match (driver presses STOP)
+
+    }
+    public void spinTurnUsingDOF(float degrees, double power, String direction) throws InterruptedException {
+
+
+        Orientation initialOrientation = imu.getAngularOrientation();
+        float angle_0 = initialOrientation.firstAngle;
+
+
+
+        // get the first value when you enter the method
+
+
+
+        // LEFT turn values always have to come in with -ve DEGREES
+        if (direction.equalsIgnoreCase("right")) {
+            degrees=angle_0+degrees;
+            while ((imu.getAngularOrientation().firstAngle < degrees) && opModeIsActive()) {
+
+                leftBack.setPower(.1);
+                leftFront.setPower(.1);
+                rightFront.setPower(-.1);
+                rightBack.setPower(-.1);
+
+                // refresh the  value inside the while loop
+
+
+
+            }
+            brake();
+            //telemetry.addData("DOF gyro value WHILE RIGHT", realOrientation);
+            telemetry.update();
+
+            // RIGHT turn values always have to come in with +ve DEGREES
+
+
+        } else if (direction.equalsIgnoreCase("left")) {
+
+            while ((imu.getAngularOrientation().firstAngle > degrees) && opModeIsActive()) {
+                leftBack.setPower(-0.1);
+                leftFront.setPower(-0.1);
+                rightFront.setPower(0.1);
+                rightBack.setPower(0.1);
+
+                // refresh the  value inside the while loop
+
+
+            }
+            brake();
+
+            //telemetry.addData("DOF gyro value WHILE LEFT", realOrientation);
+            telemetry.update();
+        }
+
+        //Stop the motors once both if statements are completed.
+        brake();
+
+    }
+    public void strafeDistRight(int leftFrontTargetDist, int leftBackTargetDist, int rightFrontTargetDist, int rightBackTargetDist) {
+
+
+        targetDist[0] = leftFrontTargetDist;
+        targetDist[1] = leftBackTargetDist;
+        targetDist[2] = rightFrontTargetDist;
+        targetDist[3] = rightBackTargetDist;
+
+        zeroPos[0] = leftFront.getCurrentPosition();
+        zeroPos[1] = leftBack.getCurrentPosition();
+        zeroPos[2] = rightFront.getCurrentPosition();
+        zeroPos[3] = rightBack.getCurrentPosition();
+
+        targetClicks[0] = (int) (targetDist[0] * TICKS_PER_INCH);
+        targetClicks[1] = (int) (targetDist[1] * TICKS_PER_INCH);
+        targetClicks[2] = (int) (targetDist[2] * TICKS_PER_INCH);
+        targetClicks[3] = (int) (targetDist[3] * TICKS_PER_INCH);
+
+        leftFront.setPower(-.14);
+        leftBack.setPower(.1);
+        rightFront.setPower(-.1);
+        rightBack.setPower(.14);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        while (opModeIsActive() && leftFront.getCurrentPosition() > zeroPos[0] - targetClicks[0] &&
+                rightBack.getCurrentPosition() < zeroPos[3] + targetClicks[3]) {
+
+            telemetry.update();
+        }
+
+
+    }
+    public void strafeDistLeft(int leftFrontTargetDist, int leftBackTargetDist, int rightFrontTargetDist, int rightBackTargetDist){
+
+        targetDist[0] = leftFrontTargetDist;
+        targetDist[1] = leftBackTargetDist;
+        targetDist[2] = rightFrontTargetDist;
+        targetDist[3] = rightBackTargetDist;
+
+        zeroPos[0] = leftFront.getCurrentPosition();
+        zeroPos[1] = leftBack.getCurrentPosition();
+        zeroPos[2] = rightFront.getCurrentPosition();
+        zeroPos[3] = rightBack.getCurrentPosition();
+
+        targetClicks[0] = (int)(targetDist[0] * TICKS_PER_INCH);
+        targetClicks[1] = (int)(targetDist[1] * TICKS_PER_INCH);;
+        targetClicks[2] = (int)(targetDist[2] * TICKS_PER_INCH);;
+        targetClicks[3]= (int)(targetDist[3] * TICKS_PER_INCH);;
+
+        leftFront.setPower(.1);
+        leftBack.setPower(-.14);
+        rightFront.setPower(.1);
+        rightBack.setPower(-.14);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        while (opModeIsActive() && leftFront.getCurrentPosition() < zeroPos[0] + targetClicks[0] &&
+                rightBack.getCurrentPosition() > zeroPos[3] - targetClicks[3]) {
+
+            telemetry.update();
+        }
+
+    }
+    public void turnRight(double degrees){
+        double dist = 12*Math.PI *(2*degrees/360);
+        double leftFrontTargetDist = -dist;
+        double leftBackTargetDist = -dist;
+        double rightFrontTargetDist = dist;
+        double rightBackTargetDist = dist;
+
+        targetDist[0] = leftFrontTargetDist;
+        targetDist[1] = leftBackTargetDist;
+        targetDist[2] = rightFrontTargetDist;
+        targetDist[3] = rightBackTargetDist;
+
+        zeroPos[0] = leftFront.getCurrentPosition();
+        zeroPos[1] = leftBack.getCurrentPosition();
+        zeroPos[2] = rightFront.getCurrentPosition();
+        zeroPos[3] = rightBack.getCurrentPosition();
+
+        targetClicks[0] = (int)(targetDist[0] * TICKS_PER_INCH);
+        targetClicks[1] = (int)((int)(targetDist[1] * TICKS_PER_INCH));
+        targetClicks[2] = (int)((int)(targetDist[2] * TICKS_PER_INCH));
+        targetClicks[3]= (int)((int)(targetDist[3] * TICKS_PER_INCH));
+
+        leftFront.setPower(-.1);
+        leftBack.setPower(-.1);
+        rightFront.setPower(.1);
+        rightBack.setPower(.1);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+
+        while(opModeIsActive() && leftFront.getCurrentPosition()<zeroPos[0]-targetClicks[0] &&
+                leftBack.getCurrentPosition()<zeroPos[1]-targetClicks[1] &&
+                rightFront.getCurrentPosition()<zeroPos[2]+targetClicks[2] &&
+                rightBack.getCurrentPosition()<zeroPos[3]+targetClicks[3]){
+
+            telemetry.addData("Left front pos: ", leftFront.getCurrentPosition());
+            telemetry.addData("Left back pos: ", leftBack.getCurrentPosition());
+            telemetry.addData("Right front pos: ", leftFront.getCurrentPosition());
+            telemetry.addData("Right back pos: ", leftFront.getCurrentPosition());
+            telemetry.update();
+
+        }
+
+    }
+    public void turnLeft(double degrees){
+        double dist = 12*Math.PI *(2*degrees/360);
+        double leftFrontTargetDist = dist;
+        double leftBackTargetDist = dist;
+        double rightFrontTargetDist = -dist;
+        double rightBackTargetDist = -dist;
+
+        targetDist[0] = leftFrontTargetDist;
+        targetDist[1] = leftBackTargetDist;
+        targetDist[2] = rightFrontTargetDist;
+        targetDist[3] = rightBackTargetDist;
+
+        zeroPos[0] = leftFront.getCurrentPosition();
+        zeroPos[1] = leftBack.getCurrentPosition();
+        zeroPos[2] = rightFront.getCurrentPosition();
+        zeroPos[3] = rightBack.getCurrentPosition();
+
+        targetClicks[0] = (int)(targetDist[0] * TICKS_PER_INCH);
+        targetClicks[1] = (int)((int)(targetDist[1] * TICKS_PER_INCH));
+        targetClicks[2] = (int)((int)(targetDist[2] * TICKS_PER_INCH));
+        targetClicks[3]= (int)((int)(targetDist[3] * TICKS_PER_INCH));
+
+        leftFront.setPower(.1);
+        leftBack.setPower(.1);
+        rightFront.setPower(-.1);
+        rightBack.setPower(-.1);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+
+        while(opModeIsActive() && leftFront.getCurrentPosition()<zeroPos[0]+targetClicks[0] &&
+                leftBack.getCurrentPosition()<zeroPos[1]+targetClicks[1] &&
+                rightFront.getCurrentPosition()<zeroPos[2]-targetClicks[2] &&
+                rightBack.getCurrentPosition()<zeroPos[3]-targetClicks[3]){
+
+            telemetry.addData("Left front pos: ", leftFront.getCurrentPosition());
+            telemetry.addData("Left back pos: ", leftBack.getCurrentPosition());
+            telemetry.addData("Right front pos: ", leftFront.getCurrentPosition());
+            telemetry.addData("Right back pos: ", leftFront.getCurrentPosition());
+            telemetry.update();
+
+        }
+
+    }
+    public void moveDistBack(int leftFrontTargetDist, int leftBackTargetDist, int rightFrontTargetDist, int rightBackTargetDist){
+
+        targetDist[0] = leftFrontTargetDist;
+        targetDist[1] = leftBackTargetDist;
+        targetDist[2] = rightFrontTargetDist;
+        targetDist[3] = rightBackTargetDist;
+
+        zeroPos[0] = leftFront.getCurrentPosition();
+        zeroPos[1] = leftBack.getCurrentPosition();
+        zeroPos[2] = rightFront.getCurrentPosition();
+        zeroPos[3] = rightBack.getCurrentPosition();
+
+        targetClicks[0] = (int)(targetDist[0] * TICKS_PER_INCH);
+        targetClicks[1] = (int)(targetDist[1] * TICKS_PER_INCH);;
+        targetClicks[2] = (int)(targetDist[2] * TICKS_PER_INCH);;
+        targetClicks[3]= (int)(targetDist[3] * TICKS_PER_INCH);;
+
+        leftFront.setPower(-.1);
+        leftBack.setPower(-.1);
+        rightFront.setPower(-.1);
+        rightBack.setPower(-.1);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+
+        while(opModeIsActive() && leftFront.getCurrentPosition()>zeroPos[0]-targetClicks[0] &&
+                leftBack.getCurrentPosition()>zeroPos[1]-targetClicks[1] &&
+                rightFront.getCurrentPosition()>zeroPos[2]-targetClicks[2] &&
+                rightBack.getCurrentPosition()>zeroPos[3]-targetClicks[3]){
+
+            telemetry.addData("Left front pos: ", leftFront.getCurrentPosition());
+            telemetry.addData("Left back pos: ", leftBack.getCurrentPosition());
+            telemetry.addData("Right front pos: ", leftFront.getCurrentPosition());
+            telemetry.addData("Right back pos: ", leftFront.getCurrentPosition());
+            telemetry.update();
+
+        }
+
+    }
+    public void moveDistForward(int leftFrontTargetDist, int leftBackTargetDist, int rightFrontTargetDist, int rightBackTargetDist){
+
+        targetDist[0] = leftFrontTargetDist;
+        targetDist[1] = leftBackTargetDist;
+        targetDist[2] = rightFrontTargetDist;
+        targetDist[3] = rightBackTargetDist;
+
+        zeroPos[0] = leftFront.getCurrentPosition();
+        zeroPos[1] = leftBack.getCurrentPosition();
+        zeroPos[2] = rightFront.getCurrentPosition();
+        zeroPos[3] = rightBack.getCurrentPosition();
+
+        targetClicks[0] = (int)(targetDist[0] * TICKS_PER_INCH);
+        targetClicks[1] = (int)(targetDist[1] * TICKS_PER_INCH);;
+        targetClicks[2] = (int)(targetDist[2] * TICKS_PER_INCH);;
+        targetClicks[3]= (int)(targetDist[3] * TICKS_PER_INCH);;
+
+        leftFront.setPower(.1);
+        leftBack.setPower(.1);
+        rightFront.setPower(.1);
+        rightBack.setPower(.1);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+
+        while(opModeIsActive() && leftFront.getCurrentPosition()<zeroPos[0]+targetClicks[0] &&
+                leftBack.getCurrentPosition()<zeroPos[1]+targetClicks[1] &&
+                rightFront.getCurrentPosition()<zeroPos[2]+targetClicks[2] &&
+                rightBack.getCurrentPosition()<zeroPos[3]+targetClicks[3]){
+
+            telemetry.addData("Left front pos: ", leftFront.getCurrentPosition());
+            telemetry.addData("Left back pos: ", leftBack.getCurrentPosition());
+            telemetry.addData("Right front pos: ", leftFront.getCurrentPosition());
+            telemetry.addData("Right back pos: ", leftFront.getCurrentPosition());
+            telemetry.update();
+
+        }
+
+    }
+    public void brake(){
+        leftFront.setPower(0.01);
+        leftBack.setPower(-0.01);
+        rightFront.setPower(0.01);
+        rightBack.setPower(-0.01);
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+    }
+}
